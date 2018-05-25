283cc7b3a1ca8b5a578ada68f8e90f7f027a8644
Matheus Roggero <mthlifebr@gmail.com>
2018-05-25 17:37:13 -0300

    segundo commit projeto c

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Projetos_C
 Conteúdo dos Projetos em Linguagem C
 feitos por mim.
+
+-Programa que Calcula Soma entre 2 Números
+-
---
+++ hello5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+ int main(void){
+ 
+ int num1, num2 , soma;
+  printf("Informe um numero: \n");
+  scanf("%i",&num1);
+
+ printf("Informe outro Numero:\n");
+  scanf("%i",&num2);
+
+  soma = (num1 + num2);
+
+  printf("soma %i:\n",soma);
+
+
+ return 0;
+
+
+}
