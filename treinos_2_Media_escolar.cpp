b12069a6cadb13384f775c957a66fea0d36c9473
Matheus Roggero <mthlifebr@gmail.com>
2018-05-25 15:22:46 -0300

    Primeiro Commit do Project

---
+++ treinos_2_Media_escolar.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main (){
+
+{
+	float nota1, nota2, nota3, nota4, nota5, media;
+
+      printf("Digite a primeira nota:");
+      scanf("%f", &nota1);
+      printf("Digite a segunda snota:");
+      scanf("%f", &nota2);
+      printf("Digite a terceira nota:");
+      scanf("%f", &nota3);
+      printf("Digite a quarta nota:");
+      scanf("%f", &nota4);
+      printf("Digite a quinta nota:");
+      scanf("%f", &nota5);
+      media=(nota1+nota2+nota3+nota4+nota5)/5;
+      printf("A media do aluno foi %f.", media);
+       	 
+}
+
+
+}
